ff96d58a06590503f648c46dc61a9327ced6df1d
Arve Svendsen <arve.svendsen@gmail.com>
2020-12-01 07:08:58 +0100

    Day 1 solution

---
+++ 01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+const string url = "https://julekalender-backend.knowit.no/challenges/1/attachments/numbers.txt";
+const int maxsize = 100000;
+
+var vs = new bool[maxsize + 1];
+vs[0] = true; // hack as as the numbers start at 1
+
+using (WebClient client = new WebClient())
+{
+    foreach (var number in client
+        .DownloadString(url)
+        .Split(',')
+        .Select(s => int.Parse(s)))
+    {
+        vs[number] = true;
+    }
+
+    Console.WriteLine(Array.IndexOf(vs, false));
+}
